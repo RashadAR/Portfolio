741bfeaf9dc91c25983708f0839a09e1df2f562c
Rashad <rashad.rar37@gmail.com>
2024-06-12 22:52:05 +0530

    svgs update

--- src/utils/data/skills.js
+++ src/utils/data/skills.js
@@ -14,7 +14,8 @@ export const skillsData = [
     'Bootstrap',
     'MaterialUI',
     'Bulma',
-    'ViteJS'
+    'ViteJS',
+    'Django'
   ]
   
   // Choose your skills from below. Make sure it's in the same format and spelled correctly.
